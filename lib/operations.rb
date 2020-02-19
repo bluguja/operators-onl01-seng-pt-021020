eb2d18129d8d1dd3c60e524c361c0a2f2bd5332e
Bala Luguja <bluguja@gmail.com>
2020-02-19 18:31:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
- def unsafe?(speed)
-  if speed > 60
-    true
-  else if speed < 40
-    true
-  else  speed >= 40 && speed <= 60
-    false
-  end
+   def unsafe?(speed)
+    if speed > 60
+      true
+     if speed < 40
+      true
+    else  speed >= 40 && speed <= 60
+      false
+    end
+    end
 end
-
 
 def not_safe?(speed)
 speed >= 40 && speed <= 60  ?  false : true
